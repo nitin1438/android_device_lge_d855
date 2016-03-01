534aaa0d15f120c951927381351c5f64d904fbb3
dadi11 <dadi.sikorski@gmail.com>
2016-03-01 13:31:08 +0100

    DU-ify
    Change-Id: I785641fe15ecc53a6a3cf3cc93346485a29a96d9

---
+++ du.mk
@@ -0,0 +1,42 @@
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from d855 device
+$(call inherit-product, device/lge/d855/full_d855.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/du/config/nfc_enhanced.mk)
+
+# Inherit some common DU stuff.
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
+
+# Boot Animation
+TARGET_SCREEN_WIDTH := 1440
+TARGET_SCREEN_HEIGHT := 2560
+
+# Set those variables here to overwrite the inherited values.
+PRODUCT_DEVICE := d855
+PRODUCT_NAME := du_d855
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LG-D855
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="g3" \
+    PRODUCT_NAME="g3_global_com" \
+    BUILD_FINGERPRINT="lge/g3_global_com/g3:6.0.1/MMB29M/2431559:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_global_com-user 6.0.1 MMB29M 2431559 release-keys"
