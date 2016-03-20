c8df3d0342fa2eadb6ffcd9cd1d3ce5dfd6c558c
Nitin chobhe <nitin1438@users.noreply.github.com>
2016-03-20 10:23:02 +0530

    Update and rename cm.mk to xosp.mk

--- xosp.mk
+++ xosp.mk
@@ -1,12 +1,13 @@
 $(call inherit-product, device/lge/d855/full_d855.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/xosp/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/xosp/config/nfc_enhanced.mk)
 
-PRODUCT_NAME := cm_d855
+PRODUCT_NAME := xosp_d855
+RELEASE_TYPE := XOSP_OFFICIAL
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
